76275575388c1ca24421dc2143f9e4e9277f99f0
wesleycrouse <wesleycrouse@gmail.com>
2018-02-05 13:14:32 -0500

    Updated help file

--- R/TIMBR_source.R
+++ R/TIMBR_source.R
@@ -79,14 +79,21 @@ model.prior.crp.marginalized <- function(m, prior.alpha.a, prior.alpha.b, scale.
 #'
 #' Posterior samples and Bayes Factors using the TIMBR model
 #'
-#' @param y phenotype vector
-#' @param prior.D diplotype prior list
-#' @param prior.M model prior list
+#' @param y vector of phenotype values for each strain
+#' @param prior.D list of inputs for the prior distribution of strain diplotype states; see data(mcv) for an example
+#' @param prior.M list of inputs for the prior distribution of the allelic series model; see data(mcv) for examples
+#' @param prior.v. shape parameter for the beta prime prior distribution on the variance component
+#' @param sample number of samples to draw from the full posterior
+#' @param samples.ml number of samples to draw from the condtiional posterior (if necessary)
+#' @param Z design matrix for covariates; first column must be a vector of ones, which is the default
+#' @param W vector of replicates for each strain; one replicate per strain by default
+#' @param verbose optionally report function progress
 #'
-#' @return None
-#'
+#' @return list of input parameters, posterior samples, and marginal likelihoods
+#' 
 #' @examples
-#' TIMBR(y, prior.D, prior.M)
+#' data(mcv)
+#' TIMBR(mcv.data$y, mcv.data$prior.D, mcv.data$prior.M$crp)
 #'
 #' @export
 TIMBR <- function(y, prior.D, prior.M, prior.v.b=1, samples=1000, samples.ml=1000, Z=NULL, W=NULL, verbose=T){
