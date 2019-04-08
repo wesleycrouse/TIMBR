f92dcfc07fabed684e4514710108c6785e8b59ac
Wesley Crouse <wesleycrouse@gmail.com>
2019-04-08 18:52:31 -0400

    Update TIMBR_source.R

--- R/TIMBR_source.R
+++ R/TIMBR_source.R
@@ -1708,11 +1708,10 @@ TIMBR2 <- function(y, prior.D, prior.M, prior.phi.b=1, samples=10000, samples.ml
           }
           
           #calculate t-distributed likelihood for all possible assignments of current row of M
-          
           if (j==j.order[1]){
-            #M.posteriors <- lapply(MC.space, nglm.hyperparameters.ml)
+            M.posteriors <- lapply(MC.space, nglm.hyperparameters.ml)
             #M.posteriors <- lapply(1:(K+1), function(x){ifelse(M.ln.prior[x]==-Inf, 0, nglm.hyperparameters.ml(MC.space[[x]]))})
-            M.posteriors <- lapply(1:(K+1), function(x){if (M.ln.prior[x]==-Inf){0} else {nglm.hyperparameters.ml(MC.space[[x]])}})
+            #M.posteriors <- lapply(1:(K+1), function(x){if (M.ln.prior[x]==-Inf){0} else {nglm.hyperparameters.ml(MC.space[[x]])}})
           } else {
             M.posteriors <- vector("list", K+1)
             M.posteriors[[M.current$new.index]] <- M.current$M.posteriors
