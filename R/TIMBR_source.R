d4a5b2caf100fa335be7449d2446e65bd54393b0
Wesley Crouse <wesleycrouse@gmail.com>
2019-05-24 19:23:09 -0400

    Update TIMBR_source.R

--- R/TIMBR_source.R
+++ R/TIMBR_source.R
@@ -1568,7 +1568,7 @@ TIMBR.scan <- function(y, prior.D.all, prior.M=NULL, prior.phi.b=1, samples=100,
 
 #' @keywords internal
 TIMBR.scan.plot <- function(TIMBR.scan.output){
-  plot.data <- c(TIMBR.scan.output$intervals[,1], TIMBR.scan.output$intervals[,2])
+  plot.data <- c(TIMBR.scan.output$intervals[,1], TIMBR.scan.output$intervals[,2]-1)
   plot.data <- cbind(plot.data, c(TIMBR.scan.output$ln.BFs, TIMBR.scan.output$ln.BFs))
   plot.data <- plot.data[order(plot.data[,1]),]
   plot(plot.data[,1], plot.data[,2], type="l")
